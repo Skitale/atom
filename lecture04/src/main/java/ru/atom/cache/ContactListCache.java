1eeca69dc170a4639952936f3d7877e3d7e558db
Aleksandr <aleksandrtolcih@gmail.com>
2018-08-28 12:35:15 +0300

    fix hw

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -1,33 +1,49 @@
 package ru.atom.cache;
 
 
+
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * What about Map?
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+    private Map<Person, List<? extends Person>> hashMap = new ConcurrentHashMap<>();
+
     public ContactListCache(int capacity) {
         super(capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new UnsupportedOperationException();
+        if(hashMap.size() == capacity){
+            removeAny();
+        }
+        hashMap.put(person, people);
+        return true;
     }
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new UnsupportedOperationException();
+        return hashMap.get(person);
     }
 
     @Override
     public int getSize() {
-        throw new UnsupportedOperationException();
+        return hashMap.size();
     }
 
     private boolean removeAny() {
-        throw new UnsupportedOperationException();
+        Random random = new Random();
+        //hashMap.remove(lastPerson);
+        Object[] personArray = hashMap.keySet().toArray();
+        int stopIndex = random.nextInt(personArray.length);
+        Person personForDelete = (Person) personArray[stopIndex];
+        hashMap.remove(personForDelete);
+        return true;
     }
 
 }
